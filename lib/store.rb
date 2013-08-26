7f81b0b295bc3d813fe72f22e0f6637a00faeee6
mose <mose@mose.com>
2013-08-26 13:03:00 +0800

    preparing populator

---
+++ lib/store.rb
@@ -0,0 +1,8 @@
+module Greeby
+  class Store
+
+    def initialize
+    end
+
+  end
+end
---
+++ lib/tasks/populate_task.rb
@@ -0,0 +1,9 @@
+require_relative '../../lib/store'
+
+namespace :populate do
+
+  desc "check mongo connection"
+  task :check do
+  end
+
+end
