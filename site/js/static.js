9e66bf40d68eaf8965432200a4d3395d709324ea
mose <mose@mose.com>
2015-05-22 04:31:36 +0800

    fix archives menu

--- site/js/static.js
+++ site/js/static.js
@@ -1,14 +1,12 @@
 (function(){
-  $(".letters li:nth-of-type(1n+13)").hide();
-  $("#show-more").on("click", function(e) {
+  var now = new Date();
+  var year = now.getFullYear();
+  $(".letters li:not(."+year+")").hide();
+  $(".show-more").on("click", function(e) {
+    show = $(this).data("year");
     e.preventDefault();
-    if ($(this).data("all") == 0) {
-      $(this).data("all",1).text("Only show last 12");
-      $(".letters li:nth-of-type(1n+13)").show();
-    } else {
-      $(this).data("all",0).text("Show all");
-      $(".letters li:nth-of-type(1n+13)").hide();
-    }
+    $(".letters li").hide();
+    $(".letters li."+show).show();
   });
   $(window).resize(function() {
     if ($(this).width() < 768) {
